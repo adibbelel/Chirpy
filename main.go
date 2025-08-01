40e818bd041875380c9e25102f85a3602bb72199
adibbelel <adibbelel1@gmail.com>
2025-08-01 18:08:38 +0100

    add custom handlers

--- main.go
+++ main.go
@@ -9,7 +9,8 @@ import (
 func main () {
 	fmt.Println("Hello World")
 	servM := http.NewServeMux()
-	servM.Handle("/assets/logo.png", http.FileServer(http.Dir(".")))
+	servM.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	servM.HandleFunc("/healthz", ContentTypeHandler)
 	server := http.Server {
 		Handler: servM,
 		Addr: ":8080",
@@ -20,4 +21,10 @@ func main () {
 		log.Fatalf("Error starting server: %v", err)
 	}
 
+}
+
+func ContentTypeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
 }
