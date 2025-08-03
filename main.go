046b92990d40f4a1a83805990ba0289fe1482b1b
adibbelel <adibbelel1@gmail.com>
2025-08-03 13:23:21 +0100

    add metric and reset handlers

--- main.go
+++ main.go
@@ -4,27 +4,51 @@ import (
 	"fmt"
 	"net/http"
 	"log"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 func main () {
-	fmt.Println("Hello World")
-	servM := http.NewServeMux()
-	servM.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	servM.HandleFunc("/healthz", ContentTypeHandler)
+	fmt.Println("Starting Server")
+	apiCfg := apiConfig {
+		fileserverHits: atomic.Int32{},
+	}
+	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("GET /healthz", ContentTypeHandler)
+	mux.HandleFunc("GET /metrics", apiCfg.metricHandler)
+	mux.HandleFunc("POST /reset", apiCfg.handlerReset)
 	server := http.Server {
-		Handler: servM,
+		Handler: mux,
 		Addr: ":8080",
 	}
 	
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Fatalf("Error starting server: %v", err)
-	}
-
+	log.Fatal(server.ListenAndServe())
 }
 
 func ContentTypeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
 	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+
+func (cfg *apiConfig) metricHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d",cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
 }
